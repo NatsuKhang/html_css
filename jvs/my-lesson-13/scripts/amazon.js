5329a6910a52457948c915e69b6a0ddb93548400
Khang <nguyenhoangkhang10022005@gmail.com>
2025-08-01 22:04:29 +0700

    update-productQuantity-selector

--- jvs/my-lesson-13/scripts/amazon.js
+++ jvs/my-lesson-13/scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class = "js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -63,6 +63,8 @@ document.querySelectorAll('.js-add-to-cart')
 
       let matchingItem;
 
+      const quantity =Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+
       cart.forEach((item) => {
         if (productId === item.productId) {
           matchingItem = item;
@@ -70,11 +72,11 @@ document.querySelectorAll('.js-add-to-cart')
       });
 
       if (matchingItem) {
-        matchingItem.quantity += 1;
+        matchingItem.quantity += quantity;
       } else {
         cart.push({
-          productId: productId,
-          quantity: 1
+          productId,
+          quantity
         });
       }
 
