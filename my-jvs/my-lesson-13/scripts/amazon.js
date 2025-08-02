f1b3fdfb3f8c5eb5e35710f7727d4ac222c0f720
Khang <nguyenhoangkhang10022005@gmail.com>
2025-08-02 23:56:26 +0700

    generate HTML for the order summary

--- my-jvs/my-lesson-13/scripts/amazon.js
+++ my-jvs/my-lesson-13/scripts/amazon.js
@@ -7,8 +7,8 @@
   - need let import-line at the top of file .js
   - need using LIVE SERVER to load the import/export ?????????? WHY ???????? WHY NOT with opened normal. 
 */
-import { cart, addToCart } from "../data/cart";
-import { products } from "../data/products";
+import { cart, addToCart } from "../data/cart.js";
+import { products } from "../data/products.js";
 
 let productsHTML = '';
 
