d233850cc3d4621995f558193d9f7a9ea716581b
Khang <nguyenhoangkhang10022005@gmail.com>
2025-08-04 15:32:41 +0700

    fix price formatting, radio selectors and add deleteFromCart()

--- my-jvs/my-lesson-13/scripts/checkout.js
+++ my-jvs/my-lesson-13/scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from "../data/cart.js"
 import { products } from "../data/products.js"
+import { formatCurrency } from "./utils/money.js"
 
 let cartSummaryHTML=""
 
@@ -28,7 +29,7 @@ cart.forEach(cartItem => {
             ${matchingItem.name}
           </div>
           <div class="product-price">
-            $${(matchingItem.priceCents/100).toFixed(2)}
+            $${formatCurrency(matchingItem.priceCents)}
           </div>
           <div class="product-quantity">
             <span>
@@ -37,7 +38,7 @@ cart.forEach(cartItem => {
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary js-delete-quatity-link" dataProductId>
               Delete
             </span>
           </div>
@@ -50,7 +51,7 @@ cart.forEach(cartItem => {
           <div class="delivery-option">
             <input type="radio" checked
               class="delivery-option-input"
-              name="delivery-option-1">
+              name="delivery-option-${matchingItem.id}">
             <div>
               <div class="delivery-option-date">
                 Tuesday, June 21
@@ -63,7 +64,7 @@ cart.forEach(cartItem => {
           <div class="delivery-option">
             <input type="radio"
               class="delivery-option-input"
-              name="delivery-option-1">
+              name="delivery-option-${matchingItem.id}">
             <div>
               <div class="delivery-option-date">
                 Wednesday, June 15
@@ -76,7 +77,7 @@ cart.forEach(cartItem => {
           <div class="delivery-option">
             <input type="radio"
               class="delivery-option-input"
-              name="delivery-option-1">
+              name="delivery-option-${matchingItem.id}">
             <div>
               <div class="delivery-option-date">
                 Monday, June 13
@@ -92,5 +93,5 @@ cart.forEach(cartItem => {
   `
 })
 
-document.querySelector('.order-summary')
+document.querySelector('.js-order-summary')
   .innerHTML=cartSummaryHTML;
